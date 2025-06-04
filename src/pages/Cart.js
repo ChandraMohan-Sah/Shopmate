ec41450dbea36754df9b0d8667c3b5f7890edee0
ChandraMohan-Sah <csah9628@gmail.com>
2025-06-04 12:39:51 +0545

    style(css): fix and unify background colors across all components

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -7,12 +7,16 @@ export const Cart = () => {
   const products = [
     {"id": 1, "name": "Sony Wh-Ch510 Bluetooth Wireless", "price": 149, "image": "/assets/images/1001.png"},
     {"id": 2, "name": "boAt Rockerz 450", "price": 49, "image": "/assets/images/1002.png"},
-  ]
+  ] 
+
+  const style={
+    color:"white"
+  }
 
   return (
     <main>
       <section className="cart">
-        <h1>Cart Items : { products.length }</h1>
+        <h1 style={style}>Cart Items : { products.length }</h1>
         {products.map((product) =>
           <CartCard key={product.id} product={product} />
         )}
